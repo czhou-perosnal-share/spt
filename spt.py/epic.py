841444e0b0ab1dfe90ec1d85a7d59195060cd014
chao.zhou <czhou@seismic.com>
2024-01-09 11:04:17 +0800

    add TargetReleaseSeason

--- spt.py/epic.py
+++ spt.py/epic.py
@@ -9,6 +9,7 @@ def get_epic_options(project, release):
         'customfield_12394': {'id':'12486'},   # Strategy Area: 'ContentDiscovery'
         'customfield_12481': {'id':'12562'},   # GroupTeam: 'Search'
         'customfield_12395': {'id':'12244'},   # ScrumTeam: 'Search'        
+        'customfield_12496': {'id':'14162'},   # TargetReleaseSeason: 'FY25Spring'
         'customfield_12491': [{'id':'12662'}], # WorkingPeriod: 'FY25Spring'
         'customfield_12492': {'id':'12699'},   # DeliveryType: 'Driving'
         'customfield_12127': '2024-02-01',     # StartDate: '2024-05-31'
