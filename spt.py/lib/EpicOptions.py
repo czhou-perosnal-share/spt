6b03aa55c4c18d31b7a85626a939ff703b11ff58
chao.zhou <czhou@seismic.com>
2024-01-10 17:13:15 +0800

    import options from existing issue.

--- spt.py/lib/EpicOptions.py
+++ spt.py/lib/EpicOptions.py
@@ -1,10 +1,12 @@
+from importlib import invalidate_caches
+from tkinter import SE
+from lib.Fields import Fields
+import json
+import jira
+
 class EpicOptions(object):
     EPIC_HEAD = '[SPT]'
-
-    def get_epic_options(project, release):
-        epic_options = {
-            'project': {'key': project},
-            'summary': EpicOptions.EPIC_HEAD + ' ' + release.fields.summary,
+    DefaultEpicOptions = {
             'description': 'built from seismic planing tools',
             'issuetype': {'name': 'Epic'},
             'customfield_12394': {'id':'12486'},   # Strategy Area: 'ContentDiscovery'
@@ -13,7 +15,50 @@ class EpicOptions(object):
             'customfield_12496': {'id':'14162'},   # TargetReleaseSeason: 'FY25Spring'
             'customfield_12491': [{'id':'12662'}], # WorkingPeriod: 'FY25Spring'
             'customfield_12492': {'id':'12699'},   # DeliveryType: 'Driving'
-            'customfield_12127': '2024-02-01',     # StartDate: '2024-05-31'
+            'customfield_12127': '2024-02-01',     # StartDate: '2024-02-01'
             'customfield_12423': '2024-05-31',     # EndDate: '2024-05-31'
         }
+
+    def get_epic_options(project, release):
+        epic_options = {
+            'project': {'key': project},
+            'summary': EpicOptions.EPIC_HEAD + ' ' + release.fields.summary,
+        }
+
+        #copy default options
+        for key in EpicOptions.DefaultEpicOptions:
+            epic_options[key] = EpicOptions.DefaultEpicOptions[key]
+        
         return epic_options   
+    
+    def __init__(self,jira):
+        self.jira = jira
+
+    def get_epic_options(self,project,release):
+        return EpicOptions.get_epic_options(project,release)
+    
+    def get_epic_options_from_issue(self, key, display_names):
+        epic_options = {}
+        fields = Fields(self.jira)
+        issue = self.jira.issue(key)
+        for name in display_names:
+            field = fields.get_field(name)
+            if field:
+                fid = field['id']
+                value = issue.fields.__dict__[fid]
+                print(type(value))
+                epic_options[fid] = self.__get_json_value(value)        
+        return epic_options
+    
+    def __get_json_value(self, value):
+        if isinstance(value,jira.resources.CustomFieldOption):
+            return value.raw
+        elif (     isinstance(value,list) 
+               and len(value) > 0 
+               and isinstance(value[0],jira.resources.CustomFieldOption)
+             ):
+            return [ v.raw for v in value ]
+        else:
+            return value
+
+            
